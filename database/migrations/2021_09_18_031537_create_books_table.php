17786924fcb8c41541d122a1bd9955bd2c55cf85
dxxnda <dindadindaaa92@gmail.com>
2021-09-18 11:45:48 +0700

    awal

---
+++ database/migrations/2021_09_18_031537_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_buku')->unique();
+            $table->integer('stok_barang');
+            $table->text('sinopsis_buku');
+            $table->foreignId('category_id')
+            ->constrained('categories')
+            ->onDelete('restrict')
+            ->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
